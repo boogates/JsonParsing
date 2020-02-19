1f7fb307f22bdedd52c645537e6812439db7f419
Brayden Gates <bmgates@bsu.edu>
2020-02-19 15:21:25 -0500

    Finished JsonParsing hands-on assignment.

--- src/main/java/utils/ParseUtils.java
+++ src/main/java/utils/ParseUtils.java
@@ -1,15 +1,35 @@
 package utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import domain.Person;
+import exceptions.ParameterIsNotJsonStringException;
 
 public class ParseUtils {
 
-    public static Person parseJsonToPersonDirect(String jsonString) {
-        return null;
+    public static Person parseJsonToPersonDirect(String jsonString) throws ParameterIsNotJsonStringException {
+        if(jsonString.startsWith("{")){
+            String sampleString = jsonString;
+            Gson gson = new Gson();
+            Person person = gson.fromJson(sampleString, Person.class);
+            return person;
+        }
+        else{
+            throw new ParameterIsNotJsonStringException();
+        }
+
     }
 
     public static Person parseJsonToPersonManual(String jsonString) {
-        return null;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(jsonString);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        var firstName = rootObject.getAsJsonPrimitive("name").getAsString();
+        var lastName = rootObject.getAsJsonPrimitive("last").getAsString();
+        var year = rootObject.getAsJsonPrimitive("dob").getAsInt();
+        return new Person(firstName, lastName, year);
     }
 
 }
